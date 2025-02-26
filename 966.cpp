454395b9dc6679d964595705d6b155fdd4d9a7aa
peter <zhangnianhao@ruc.edu.cn>
2025-02-26 20:12:42 +0800

    ?

--- 966.cpp
+++ 966.cpp
@@ -69,11 +69,6 @@ int main()
         stack[mul[i]] %= MOD;
         stack[mul[i] - 1] = 0;
     }
-    // for(int i = 0; i < top; i++)
-    // {
-    //     cout << stack[i] << " ";
-    // }
-    // cout << endl;
     for (int i = 0; i < top; i++)
     {
         ans += stack[i];
